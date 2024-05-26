04a239f6992eede3c5d08c7d534cfc99d9156e1c
alejostrvz <alejandrostradavz@gmail.com>
2024-05-25 20:12:58 -0600

    commit inicial

---
+++ formdata.js
@@ -0,0 +1,30 @@
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    if (response.ok){
+        this.reset()
+        showToast('Gracias por contactarme!!');
+    } else {
+        showToast('Hubo un error al enviar el formulario');
+    }
+}
+
+function showToast(message) {
+    const toast = document.getElementById('toast');
+    toast.textContent = message;
+    toast.className = 'toast show';
+    setTimeout(() => {
+        toast.className = toast.className.replace('show', '');
+    }, 3000);
+}
